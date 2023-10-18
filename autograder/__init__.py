67c8fa8a055097ef125172b0d95a877a99237136
jmart5 <93228623+jmart5@users.noreply.github.com>
2023-10-18 07:59:42 -0400

    Modified project title

--- autograder/__init__.py
+++ autograder/__init__.py
@@ -2,7 +2,7 @@ import importlib.metadata
 
 __version__ = importlib.metadata.version("autograder")
 
-__title__ = "autograder"
+__title__ = "autograderModV1"
 __description__ = "Automatic assignment grading for instructor use in programming courses"
 __author__ = "Stanislav Zmiev"
 __author_email__ = "zmievsa@gmail.com"
